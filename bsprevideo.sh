95f79a69a9edd4aef81f27477d5b8f4248f1244d
marv <52173961+rain874@users.noreply.github.com>
2020-09-30 11:16:27 +0200

    Update bsprevideo.sh

--- bsprevideo.sh
+++ bsprevideo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Die for Schleife sucht im Ordner nach datein mit der Endung .mp4
-# mit dem ffmpeg (Audio-und -Video-Encoder und -Decoder) wird dann aus allen datein ein Video von 180 sek geschnitten das Video wird ab sek 15 gestartet und hat eine Auflösung von 354x200
-# dann wird das Video mit seinen eigentlichen Namen und dem Zusatz -thumb abgespeichert
+# The for loop searches for files with the extension .mp4 in the folder
+# With the ffmpeg (audio and video encoder and decoder) a video of 180 seconds is then cut from all files. The video is started from 15 seconds and has a resolution of 354x200
+# then the video is saved with its actual name and the addition -thumb
 for name in *.mp4; do
 ffmpeg -ss 180 -i "$name" -t 15 -s 354x200 -filter:v "setpts=0.5*PTS" -an "${name%.*}-thumb.mp4"
 done
